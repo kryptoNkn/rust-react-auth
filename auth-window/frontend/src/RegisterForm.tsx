6a12af6cd39a90197d6efacf4492823555f1f719
kryptoNkn <andrej.art3mov@yandex.ru>
2025-09-04 00:48:53 +0300

    Remove unused Toastify CSS import
    Deleted the import of 'react-toastify/dist/ReactToastify.css' from RegisterForm.tsx as it is not required.

--- auth-window/frontend/src/RegisterForm.tsx
+++ auth-window/frontend/src/RegisterForm.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
 import s from './RegisterForm.module.scss';
 
 export default function RegisterForm() {
